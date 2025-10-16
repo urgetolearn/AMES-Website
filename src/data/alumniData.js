4bd31d7293740fc86e0481349dceef4bd5f11b0b
Adarsh S <adarshs2500@gmail.com>
2025-10-16 19:25:20 +0000

    minor changes

--- src/data/alumniData.js
+++ src/data/alumniData.js
@@ -2,27 +2,27 @@ import instaIcon from '../assets/instaicon.png';
 import LinkedInIcon from '../assets/linkedin.png';
 const alumniData = [
   {
-    name: "NAME",
+    name: "KV Kamath",
     role: "Role/Position",
-    intro: "Brief intro about this alumni goes here.lorem edfawfj edtpgjasgqaeg weojadsfgvaweojtg dsfjoagzbnkag sdjhoagagnag lorem dfskghsdfh dfkghsdh dfhbsdhbos",
+    intro: "A prominent Indian banker, KV Kamath is the Chairman of Jio Financial Services. He previously led the New Development Bank of BRICS and had an influential career as the MD & CEO of ICICI Bank, where he was pivotal in transforming India's financial services landscape.",
     instaicon: instaIcon,
     instalink: "#",
     linkedinicon: LinkedInIcon,
-    linkedinlink: "#"
+    linkedinlink: "https://www.linkedin.com/in/k-v-kamath-b8683510/"
   },
   {
-    name: "NAME",
+    name: "K Ullas Karanth",
     role: "Role/Position",
-    intro: "Brief intro about this alumni goes here.lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    intro: "Dr. K. Ullas Karanth is a world-renowned conservation zoologist and a leading authority on tigers. As the Director Emeritus at the Centre for Wildlife Studies, he pioneered the use of camera traps for wildlife population studies, revolutionizing conservation science.",
     instaicon: instaIcon,
     instalink: "#",
     linkedinicon: LinkedInIcon,
-    linkedinlink: "#"
+    linkedinlink: "https://www.linkedin.com/in/k-ullas-karanth-486161a/"
   },
   {
-    name: "NAME",
+    name: "Dr. Hirebettu Sadananda Kamath",
     role: "Role/Position",
-    intro: "Brief intro about this alumni goes here.",
+    intro: "Dr. H. S. Kamath is a distinguished nuclear scientist with a significant career at the Department of Atomic Energy (DAE). He has made substantial contributions to India's nuclear fuel cycle, particularly in the development and manufacturing of fuels for various nuclear reactors.",
     instaicon: instaIcon,
     instalink: "#",
     linkedinicon: LinkedInIcon, 
